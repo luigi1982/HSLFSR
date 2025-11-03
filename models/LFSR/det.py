fixed attention computation in AngTrans

models/LFSR/det.py
@@ -116,7 +116,7 @@ class AngTrans(nn.Module):
         )
 
         attn = F.softmax(self.scale * einsum(q, k, 'b h l1 d, b h l2 d -> b h l1 l2'), dim=-1)
-        buffer = einsum(attn, v, 'b h l1 l2, b h l1 d -> b h l1 d')
+        buffer = einsum(attn, v, 'b h l1 l2, b h l2 d -> b h l1 d')
 
         buffer = rearrange(buffer, 'b h l d -> b l (h d)')
         buffer = self.w_out(buffer)

models/LFSR/lft.py
@@ -12,10 +12,10 @@ class SinusoidalPosEmb(nn.Module):
 
     def forward(self, x):
         device = x.device
-        pos = torch.arange(x.size(1), device=device)
-        chn = torch.arange(self.dim//2, device=device)
+        pos = torch.arange(x.size(1), device=device).float() 
+        chn = torch.arange(self.dim//2, device=device).float() 
         chn = chn * self.a/(self.dim//2)
-        emb = pos[:, None] / chn[None, :]
+        emb = pos[:, None] / torch.exp(chn)[None, :]
         return torch.cat([emb.sin(), emb.cos()], dim=-1)[None, :, :]
     
 class SinusoidalPosEmb2d(nn.Module):
@@ -31,10 +31,10 @@ class SinusoidalPosEmb2d(nn.Module):
         _, l, _ = x.size()
         device = x.device
         h = int(math.sqrt(l))
-        pos = torch.arange(h, device=device)
-        chn = torch.arange(self.dim//2, device=device)
+        pos = torch.arange(h, device=device).float() 
+        chn = torch.arange(self.dim//2, device=device).float() 
         chn = chn * self.a/(self.dim//2)
-        emb = pos[:, None] / chn[None, :]
+        emb = pos[:, None] / torch.exp(chn)[None, :]
         emb_cos = emb.cos()
         emb_sin = emb.sin()
 
@@ -203,23 +203,23 @@ class LFT(nn.Module):
 
     def forward(self, x, scale=4):
         #input B x UV x H x W
+        
+        #initial
+        buffer = x.unsqueeze(1)
+        buffer = self.conv_init0(buffer)
+        buffer = buffer + self.conv_init(buffer)
+
+        #body
+        buffer = rearrange(buffer, 'b c n h w -> b (h w) n c')
+        buffer = buffer + self.body(buffer)
+
+        buffer = rearrange(buffer, 'b (h w) (u v) c -> b c (u h) (v w)', h=32, v=5)
+
+        #upscale
+        buffer = self.upsampling(buffer)
+        buffer = rearrange(buffer, ' b c (u h) (v w) -> (b c) (u v) h w', h=scale*32, v=5)
 
         #bc interpolate lr
         bc = F.interpolate(x, scale_factor=scale, mode='bicubic')
-        
-        #initial
-        x = x.unsqueeze(1)
-        buffer = self.conv_init0(x)
-        x = x + self.conv_init(buffer)
 
-        #body
-        x = rearrange(x, 'b c n h w -> b (h w) n c')
-        x = x + self.body(x)
-
-        x = rearrange(x, 'b (h w) (u v) c -> b c (u h) (v w)', h=32, v=5)
-
-        #upscale
-        x = self.upsampling(x)
-        x = rearrange(x, ' b c (u h) (v w) -> (b c) (u v) h w', h=scale*32, v=5)
-
-        return x + bc
+        return buffer + bc

result_vis/get_lightfields.py
@@ -10,7 +10,7 @@ def get_lfsr_paths(model, exp_name):
 
     scene_paths = []
 
-    path=os.path.join('results', model, exp_name)
+    path=os.path.join('results', model, 'training', exp_name)
     epochs=os.listdir(path)
     nums=[]
     for epoch in epochs:
@@ -32,7 +32,8 @@ def get_lfsr_paths(model, exp_name):
 def get_lfhr_paths():
     scene_paths = []
     path='../datasets'
-    for test in os.listdir(path):
+    for test in ['Lab_day', 'Lab_night', 'Indoors_day', 'Indoors_night', 'Showroom', 'Outdoors']: #for test in os.listdir(path):
+        print(test)
         test = os.path.join(path, test, 'test_hsi')
         scene=os.listdir(test)[0]
         scene=os.path.join(test, scene)
@@ -44,6 +45,7 @@ def load_lfs(paths, key):
     lfs = []
     for path in paths:
         with h5py.File(path, 'r') as hf:
+            print(hf.keys())
             LF = np.array(hf.get(key))
         lfs.append(LF)
 
@@ -54,18 +56,18 @@ def plot_central_view(model, exp_name, srs, hrs):
     _, axs = plt.subplots(len(srs), 3, figsize=(6, 10), constrained_layout=True)
     for i, (sr, hr) in enumerate(zip(srs, hrs)):
         sr_cv = sr[12]
-        hr_cv = np.transpose(hr, (2, 0, 1))[12]
+        hr_cv = hr[12]
 
         #apply first normalization and clamping to HR image
         #revert second normalization on SR image
 
-        mean1 = 0.0961
+        '''mean1 = 0.0961
         std1 = 0.1125
         mean2 = -0.1483
         std2 = 0.6535
 
         hr_cv = (hr_cv - mean1) / std1
-        sr_cv = std2*sr_cv + mean2
+        sr_cv = std2*sr_cv + mean2'''
 
         '''_, axs1 = plt.subplots(1, 2)
         hr_u8 = (255*(hr_cv + 1)/2).astype(np.uint8)
@@ -78,8 +80,6 @@ def plot_central_view(model, exp_name, srs, hrs):
 
         plt.show()
         plt.savefig(f'bins_{i}.png')'''
-
-        print('Min - Max', sr_cv.min(), sr_cv.max())
 
         bc = torch.from_numpy(hr_cv).unsqueeze(0).unsqueeze(0)
         bc = F.interpolate(bc, scale_factor=0.25, mode='bicubic')
@@ -105,10 +105,13 @@ def plot_central_view(model, exp_name, srs, hrs):
 def create_qual_plot(model, exp_name):
         
     p2=get_lfsr_paths(model, exp_name)
+    sigma = [4, 2, 0, 3, 1, 5]
+    pr = [p2[i] for i in sigma]
+    p2 = pr
     lfs_sr=load_lfs(p2, 'SR')
     p1=get_lfhr_paths()
-    p1 = p1 if len(lfs_sr) == 6 else p1[1:]
-    lfs_hr=load_lfs(p1, 'LF')
+    print(p1, p2)
+    lfs_hr=load_lfs(p1, 'HR')
     path = plot_central_view(model, exp_name, lfs_sr, lfs_hr)
 
     return path

sisr_/dataset.py
@@ -63,7 +63,7 @@ class LightFieldTestDataset(Dataset):
     def __getitem__(self, index):
 
         with h5py.File(self.file_list[index], 'r') as hf:
-            LF = np.array(hf.get('LF'))
+            LF = np.array(hf.get('HR'))
 
         #LF = torch.from_numpy(LF).permute((2, 0, 1)).to(torch.float32)
 

tasks/sisr.py
@@ -62,7 +62,7 @@ class SISRTrainer(Trainer):
                     with torch.no_grad():
                         self.model.eval()
                         torch.cuda.empty_cache()
-                        out = self.model(sais.to(self.device), wl)
+                        out = self.model(sais.to(self.device)) #self.model(sais.to(self.device), wl)
                         outs.append(out)
             
             sub_LF_out.append(torch.cat(outs, dim=0).view((1, 25, 128, 128)))